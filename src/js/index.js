992a67d3475fe07da2eae6c1e4519c685a2256d7
crduque <crduque@hotmail.com>
2020-10-25 15:03:13 +0000

    created the array for sorting

--- src/js/index.js
+++ src/js/index.js
@@ -20,6 +20,8 @@ const values = [
   13 //si es 13, imprime K
 ];
 const suits = ["hearts", "spades", "clubs", "diamonds"];
+var myArray = [];
+var myArraySlice = myArray.slice(0, myArray.length);
 
 // FUNCION QUE GENERA RANDOM INDEX
 function randomIndex(array) {
@@ -63,15 +65,6 @@ function selectValue(mid) {
   mid.innerHTML = result;
   return resultOut;
 }
-// FUNCION QUE GENERA ARRAY DE LOS VALUES
-function cardGenerator() {
-  let myInput = document.querySelector("#myInput").value;
-  let father = document.querySelector("#row2");
-  father.textContent = ""; //vacia al padre si ya tiene algun contenido, para que no se sobreescriban cartas
-  for (let numberOfCards = 1; numberOfCards <= myInput; numberOfCards++) {
-    father.appendChild(createContent(numberOfCards)); //genera la carta con nuestra funcion create content y la asigna en la posicion del index del array
-  }
-}
 // FUNCION QUE CREA LOS DIVS QUE CONTIENEN LA CARTA Y SUS CLASES
 function createContent(index) {
   //creamos hijos
@@ -99,13 +92,27 @@ function createContent(index) {
   grandChild2.setAttribute("class", "row");
   grandChild3.setAttribute("class", "row");
   //asignamos clases e id al hijo
-  child.setAttribute("class", "card col-1 w-75");
+  child.setAttribute("class", "card col-1 w-100");
   child.setAttribute("id", "card" + index);
 
   selectSuit(greatGrandSon1, greatGrandSon3); //asignamos palo a la carta
-  selectValue(greatGrandSon2); //asinamos valor a la carta
+
+  myArray.push(selectValue(greatGrandSon2)); //asinamos valor a la carta y se lo añadimos a un array
+  //   console.log(myArray);
 
   return child;
 }
-
+// FUNCION QUE GENERA CADA CARTA EN FUNCION DEL NUMERO DEL INPUT Y EL ARRAY CON LOS VALORES
+function cardGenerator() {
+  let myInput = document.querySelector("#myInput").value;
+  let father = document.querySelector("#row2");
+  father.textContent = ""; //vacia al padre si ya tiene algun contenido, para que no se sobreescriban cartas
+  for (let numberOfCards = 1; numberOfCards <= myInput; numberOfCards++) {
+    father.appendChild(createContent(numberOfCards));
+  }
+  //aqui utilizamos el array para el fin que queramos
+  console.log(myArray);
+  //aqui lo vaciamos para la siguiente vuelta (la siguiente vez que generes unas cartas)
+  myArray.splice(0, myArray.length);
+}
 drawButton.addEventListener("click", cardGenerator);
