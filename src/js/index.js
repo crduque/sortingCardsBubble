c71b5ad08fe1fd7ffcd2581ec3f740f785771fcc
crduque <crduque@hotmail.com>
2021-01-09 15:43:41 +0000

    added sorting algorithms and its functionality with the cards

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,16 @@ import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
+// asignando botones a variables
 const drawButton = document.querySelector("#drawButton");
 const bubbleSortButton = document.querySelector("#bubbleSortButton");
+const selectSortButton = document.querySelector("#selectSortButton");
+
+// asignándoles funcionalidad a los botones
+selectSortButton.addEventListener("click", select);
+bubbleSortButton.addEventListener("click", bubble);
+drawButton.addEventListener("click", cardGenerator);
+
 const values = [
   1, //si es 1, imprime A en vez de 1
   2,
@@ -20,17 +28,18 @@ const values = [
   13 //si es 13, imprime K
 ];
 const suits = ["hearts", "spades", "clubs", "diamonds"];
-var myArray = [];
-var myArraySlice = myArray.slice(0, myArray.length);
+
+var myArray = []; // array que contendrá información sobre el palo y valor de las cartas generadas
 
 // FUNCION QUE GENERA RANDOM INDEX
 function randomIndex(array) {
   let randomNumber = Math.floor(Math.random() * array.length);
   return array[randomNumber];
 }
+
 // FUNCION QUE GENERA SUITS
-function selectSuit(top, bot) {
-  let result = randomIndex(suits);
+function selectSuit(result, top, bot) {
+  if (result == "") result = randomIndex(suits);
   if (result == "hearts") {
     top.className = "hearts col-4 left-suit d-flex justify-content-center";
     bot.className =
@@ -48,10 +57,12 @@ function selectSuit(top, bot) {
     bot.className =
       "diamonds-rotate col-4 offset-8 left-suit d-flex justify-content-center";
   }
+  return result;
 }
+
 // FUNCION QUE GENERA VALUE
-function selectValue(mid) {
-  let result = randomIndex(values);
+function selectValue(result, mid) {
+  if (result == 0) result = randomIndex(values);
   let resultOut = result;
   if (result == 1) {
     result = "A";
@@ -65,8 +76,9 @@ function selectValue(mid) {
   mid.innerHTML = result;
   return resultOut;
 }
+
 // FUNCION QUE CREA LOS DIVS QUE CONTIENEN LA CARTA Y SUS CLASES
-function createContent(index) {
+function createContent(content) {
   //creamos hijos
   let child = document.createElement("div");
   let grandChild1 = document.createElement("div");
@@ -93,47 +105,90 @@ function createContent(index) {
   grandChild3.setAttribute("class", "row");
   //asignamos clases e id al hijo
   child.setAttribute("class", "card col-1 w-100");
-  child.setAttribute("id", "card" + index);
+  child.setAttribute("id", "card");
 
-  selectSuit(greatGrandSon1, greatGrandSon3); //asignamos palo a la carta
-
-  myArray.push(selectValue(greatGrandSon2)); //asinamos valor a la carta y se lo añadimos a un array
-  //   console.log(myArray);
+  if (content == "") {
+    let emptySuit = "";
+    let emptyValue = 0;
+    let suitasd = selectSuit(emptySuit, greatGrandSon1, greatGrandSon3); //asignamos palo a la carta
+    let value = selectValue(emptyValue, greatGrandSon2); //asinamos valor a la carta
+    myArray.push({ suit: suitasd, value: value }); //añadimos toda la info de cada carta en un array
+  } else {
+    selectSuit(content.suit, greatGrandSon1, greatGrandSon3);
+    selectValue(content.value, greatGrandSon2);
+  }
 
   return child;
 }
+
 // FUNCION QUE GENERA CADA CARTA EN FUNCION DEL NUMERO DEL INPUT Y EL ARRAY CON LOS VALORES
 function cardGenerator() {
+  myArray = []; //vaciamos el array para la siguiente vuelta
   let myInput = document.querySelector("#myInput").value;
   let father = document.querySelector("#row2");
-  father.textContent = ""; //vacia al padre si ya tiene algun contenido, para que no se sobreescriban cartas
-  for (let numberOfCards = 1; numberOfCards <= myInput; numberOfCards++) {
-    father.appendChild(createContent(numberOfCards));
-  }
-  //aqui utilizamos el array para el fin que queramos
-  console.log(bubbleSort(myArray));
-  //   bubbleSortButton.addEventListener("click", bubbleSort(myArray));
-  //aqui lo vaciamos para la siguiente vuelta (la siguiente vez que generes unas cartas)
-  myArray.splice(0, myArray.length);
-}
-drawButton.addEventListener("click", cardGenerator);
+  father.textContent = "Generating random cards"; //texto que tiene el padre
 
-function bubbleSort(array) {
-  var swapp;
-  var n = array.length - 1;
-  var x = array;
-  do {
-    swapp = false;
-    for (var index = 0; index < n; index++) {
-      if (x[index] < x[index + 1]) {
-        var temp = x[index];
-        x[index] = x[index + 1];
-        x[index + 1] = temp;
-        swapp = true;
-        console.log(x);
+  for (let numberOfCards = 1; numberOfCards <= myInput; numberOfCards++) {
+    let empty = "";
+    father.appendChild(createContent(empty));
+  }
+}
+
+//FUNCIÓN QUE PINTA LAS CARTAS ORDENADAS
+function sortingCardsGenerator(array) {
+  let child = document.createElement("div");
+  child.setAttribute("class", "rowSort row");
+
+  for (let index = 0; index < array.length; index++) {
+    child.appendChild(createContent(array[index]));
+  }
+  return child;
+}
+
+// FUNCIÓN QUE ORDENA CON ALGORITMO BUBBLE
+const bubbleSort = () => {
+  let bubbleArray = myArray;
+  let wall = bubbleArray.length - 1;
+  let father = document.querySelector("#row3");
+  father.textContent = "Sorting cards with Bubble algorithm ";
+  while (wall > 0) {
+    let index = 0;
+    while (index < wall) {
+      if (bubbleArray[index].value > bubbleArray[index + 1].value) {
+        let aux = bubbleArray[index];
+        bubbleArray[index] = bubbleArray[index + 1];
+        bubbleArray[index + 1] = aux;
+      }
+      index++;
+    }
+    father.appendChild(sortingCardsGenerator(bubbleArray));
+    wall--;
+  }
+};
+
+function bubble() {
+  bubbleSort();
+}
+
+// FUNCIÓN QUE ORDENA CON ALGORITMO SELECT
+const selectSort = () => {
+  let selectArray = myArray;
+  let min = 0;
+  let father = document.querySelector("#row3");
+  father.textContent = "Sorting cards with Select algorithm ";
+  while (min < selectArray.length - 1) {
+    for (let i = min + 1; i < selectArray.length; i++) {
+      if (selectArray[min].value > selectArray[i].value) {
+        let aux = selectArray[min];
+        selectArray[min] = selectArray[i];
+        selectArray[i] = aux;
       }
     }
-    n--;
-  } while (swapp);
-  return x;
+    father.appendChild(sortingCardsGenerator(selectArray));
+    min++;
+  }
+};
+
+function select() {
+  selectSort();
 }
